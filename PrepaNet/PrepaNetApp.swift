c9ae2a6a2a969780675dd31de6178fcf8c51a865
Yoltic2 <eyolticmg@icloud.com>
2022-09-30 15:22:47 -0500

    Initial Commit

---
+++ PrepaNet/PrepaNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepaNetApp.swift
+//  PrepaNet
+//
+//  Created by Emilio Y Martinez on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PrepaNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
